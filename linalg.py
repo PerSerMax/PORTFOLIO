e55651e7da1323543d069f97a2f7a33aea69184e
PerSerMax <73576933+PerSerMax@users.noreply.github.com>
2023-02-17 06:06:17 +0300

    Add files via upload
    output fix

--- linalg.py
+++ linalg.py
@@ -15,7 +15,26 @@ class Misc:
         return output_list
 
     @staticmethod
-    def helloWorld():
+    def input_matrix(matrix):
+        lines = []
+        line = [int(i) for i in input(' ').rstrip().lstrip().split(' ')]
+        lines.append(line)
+        matrix.num_of_columns = len(lines[0])
+        while True:
+            line = input(' ').split(' ')
+            if line != ['']:
+                line = [int(i) for i in line]
+            else:
+                break
+            if len(line) != matrix.num_of_columns:
+                print('MatrixSizeError')
+                sys.exit()
+            lines.append(line)
+        matrix.num_of_lines = len(lines)
+        matrix.matrix = lines
+
+    @staticmethod
+    def hello_world():
         print \
             ("""
 inv(input_list) - количество инверсий
@@ -23,7 +42,8 @@ scalMul(vector_1, vector_2) - скалярное произведение век
 tranp(matrix) - транспонирует матрицу
 det(matrix) - определитель матрицы
 rank(matrix) - ранг матрицы
-Matrix(values, num_of_lines, num_of_columns) - создание матрицы, где values - значения матрицы, num_of_lines - число строк, num_of_columns - число столбцов
+Matrix(values, num_of_lines, num_of_columns) - создание матрицы, 
+где values - значения матрицы, num_of_lines - число строк, num_of_columns - число столбцов
         """)
 
 
@@ -39,7 +59,7 @@ class Calc:
         return inversions
 
     @staticmethod
-    def scalMul(vector_1, vector_2):
+    def scal_mul(vector_1, vector_2):
         su = 0
         if len(vector_1) != len(vector_2):
             print("I can't prod these vectors")
@@ -49,12 +69,12 @@ class Calc:
         return su
 
     @staticmethod
-    def tranp(matrix):
+    def transpose(matrix):
         values = Misc.unpack([matrix.col(i) for i in range(matrix.num_of_columns)])
         return Matrix(values, matrix.num_of_columns, matrix.num_of_lines)
 
     @staticmethod
-    def __minor(matrix, lines, columns):
+    def minor(matrix, lines, columns):
         lines = sorted(list(lines))
         columns = sorted(list(columns))
         minor = Matrix(range(len(lines) * len(columns)), len(lines), len(columns))
@@ -64,7 +84,7 @@ class Calc:
         return minor
 
     @staticmethod
-    def __algDop(matrix, lines, columns):
+    def alg_dop(matrix, lines, columns):
         lines = sorted(set(range(matrix.num_of_lines)) - set(lines))
         columns = sorted(set(range(matrix.num_of_columns)) - set(columns))
         minor = Matrix(range(len(lines) * len(columns)), len(lines), len(columns))
@@ -80,55 +100,59 @@ class Calc:
             sys.exit()
         if matrix.num_of_columns == 1:
             return matrix[0][0]
+        elif matrix.num_of_columns == 2:
+            return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
         else:
             su = 0
             for i in range(matrix.num_of_lines):
-                su += matrix[0][i] * Calc.det(Calc.__algDop(matrix, [0], [i])) * (-1 if i % 2 else 1)
+                if matrix[0][i] == 0:
+                    continue
+                su += matrix[0][i] * Calc.det(Calc.alg_dop(matrix, [0], [i])) * (-1 if i % 2 else 1)
             return su
 
     @staticmethod
-    def rank(matrix):
+    def rank(matrix, size=1):
         if matrix.num_of_columns * matrix.num_of_lines == 0:
-            return 0, 0
-        if matrix.num_of_columns == matrix.num_of_lines and Calc.det(matrix) != 0:
-            return matrix, matrix.num_of_columns
-        list_of_lines = list(
-            itertools.combinations(range(matrix.num_of_lines), max(matrix.num_of_columns, matrix.num_of_lines) - 1))
-        list_of_columns = list(
-            itertools.combinations(range(matrix.num_of_columns), max(matrix.num_of_columns, matrix.num_of_lines) - 1))
-        list_of_minors = []
+            return None
+        list_of_lines = itertools.combinations(range(matrix.num_of_lines), size)
+        list_of_columns = itertools.combinations(range(matrix.num_of_columns), size)
         for i in list_of_lines:
             for j in list_of_columns:
-                minor = Calc.__minor(matrix, i, j)
+                minor = Calc.minor(matrix, i, j)
                 if Calc.det(minor) != 0:
-                    return minor, minor.num_of_columns
-                list_of_minors.append(minor)
-        max_rank = (0, 0)
-        for i in list_of_minors:
-            rank = Calc.rank(i)
-            if rank[1] > max_rank[1]:
-                max_rank = rank
-        return max_rank
+                    rank = Calc.rank(matrix, size + 1)
+                    if rank != 0:
+                        return rank
+                    else:
+                        return size, minor
+        return 0
 
 
 class Matrix:
-    def __init__(self, values, num_of_lines, num_of_columns=None):
-        self.num_of_lines, self.num_of_columns = num_of_lines, num_of_columns or num_of_lines
-        self.matrix = [[] for _ in range(self.num_of_lines)]
+    def __init__(self, values=None, num_of_lines=None, num_of_columns=None):
+        if values is None and num_of_lines is None and num_of_columns is None:
+            Misc.input_matrix(self)
+        else:
+            self.num_of_lines, self.num_of_columns = num_of_lines, num_of_columns or num_of_lines
+            self.matrix = [[] for _ in range(self.num_of_lines)]
 
-        if len(values) != self.num_of_lines * self.num_of_columns:
-            print('MatrixSizeError')
-            sys.exit()
+            if len(values) != self.num_of_lines * self.num_of_columns:
+                print('MatrixSizeError')
+                sys.exit()
 
-        for i in range(self.num_of_lines):
-            for j in range(self.num_of_columns):
-                self.matrix[i].append(values[self.num_of_columns * i + j])
+            for i in range(self.num_of_lines):
+                for j in range(self.num_of_columns):
+                    self.matrix[i].append(values[self.num_of_columns * i + j])
 
     def __str__(self):
         output = str()
-        for i in self.matrix:
-            for j in i:
-                output += str(j) + '\t'
+        max_len = max([len(str(i)) for i in Misc.unpack(self.matrix)])
+        for line in self.matrix:
+            for j in line:
+                if j >= 0:
+                    output += ' ' + str(j) + ' ' * (max_len - len(str(j)))
+                else:
+                    output += str(j) + ' ' * (1 + max_len - len(str(j)))
             output += '\n'
         return output + '\n'
 
@@ -140,6 +164,21 @@ class Matrix:
 
     def __setitem__(self, key, value):
         self.matrix[key] = value
+
+    def __getattr__(self, item):
+        if item == 'rank':
+            self.rank = Calc.rank(self)[0]
+            return self.rank
+        elif item == 'det':
+            if self.rank < self.num_of_columns:
+                self.det = 0
+                return 0
+            else:
+                self.det = Calc.det(self)
+                return self.det
+        elif item == 'tran':
+            self.tran = Calc.transpose(self)
+            return self.tran
 
     def col(self, key):
         return [i[key] for i in self.matrix]
@@ -192,7 +231,7 @@ class Matrix:
                             other.num_of_columns)
             for i in range(self.num_of_lines):
                 for j in range(other.num_of_columns):
-                    matrix[i][j] = Calc.scalMul(self.line(i), other.col(j))
+                    matrix[i][j] = Calc.scal_mul(self.line(i), other.col(j))
             return matrix
         elif isinstance(other, int) or isinstance(other, float):
             matrix = Matrix(range(self.num_of_lines * self.num_of_columns), self.num_of_lines,
