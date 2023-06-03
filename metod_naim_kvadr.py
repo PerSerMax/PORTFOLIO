6d794405403b27fbe641a7da945ab60e7e0b74ce
Max <wundel.uber@yandex.ru>
2023-06-03 14:46:57 +0300

    2

--- metod_naim_kvadr.py
+++ metod_naim_kvadr.py
@@ -1,36 +1,36 @@
 import numpy as np
 
+param = int(input())
+sum_l = (param - 1) * 2
 
 x = input()
 x = x.split(' ')
-x = [int(i) for i in x]
+x = [float(i) for i in x]
 
 y = input()
 y = y.split(' ')
-y = [int(i) for i in y]
+y = [float(i) for i in y]
 
 n = len(x)
 
-s1 = sum([i for i in x])
-s2 = sum([i**2 for i in x])
-s3 = sum([i**3 for i in x])
-s4 = sum([i**4 for i in x])
-s5 = sum([i**5 for i in x])
-s6 = sum([i**6 for i in x])
-s7 = sum([i**7 for i in x])
-s8 = sum([i**8 for i in x])
+s = []
+for i in range(1, sum_l + 1):
+    s.append(sum([j**i for j in x]))
+s.reverse()
+s.append(n)
 
-p1 = sum([j for (i, j) in zip(x, y)])
-p2 = sum([i * j for (i, j) in zip(x, y)])
-p3 = sum([i**2 * j for (i, j) in zip(x, y)])
-p4 = sum([i**3 * j for (i, j) in zip(x, y)])
-p5 = sum([i**4 * j for (i, j) in zip(x, y)])
+p = []
+for i in range(param):
+    p.append(sum([a**i * b for (a, b) in zip(x, y)]))
+p.reverse()
 
-expr = np.array([s8, s7, s6, s5, s4, s7, s6, s5, s4, s3, s6, s5, s4, s3, s2, s5, s4, s3, s2, s1, s4, s3, s2, s1, n])
-expr = expr.reshape((5, 5))
-fcol = np.array([p5, p4, p3, p2, p1])
+matr = []
+for i in range(param):
+    matr.append(s[i:i+param])
 
-abcde = np.linalg.solve(expr, fcol)
+expr = np.array(matr)
+
+abcde = np.linalg.solve(expr, p)
 
 print(abcde)
 
