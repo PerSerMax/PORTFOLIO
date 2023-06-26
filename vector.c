646ad95dfa203113dfe609845bb6a8121b28c5af
PerSerMax <73576933+PerSerMax@users.noreply.github.com>
2023-06-26 11:57:28 +0300

    Update vector.c

--- vector.c
+++ vector.c
@@ -39,8 +39,27 @@ void push_back(vector* v, V_TYPE d) {
 	v->loaded += 1;
 }
 
+void push_fwd(vector* v, V_TYPE d) {
+	if (v->loaded + 1 > v->length) {
+		V_TYPE* new_data = (V_TYPE*)realloc(v->v_data, 2 * sizeof(V_TYPE) * ((unsigned long long)(v->length)));
+		if (!new_data) {
+			printf("REALLOC ERROR");
+			return;
+		}
+		v->length *= 2;
+		v->v_data = new_data;
+	}
+
+	for (size_t i = v->loaded; i > 0; i--)
+	{
+		v->v_data[i] = v->v_data[i - 1];
+	}
+	v->v_data[0] = d;
+	v->loaded += 1;
+}
+
 vector to_vector(unsigned long long len, V_TYPE* data) {
-	vector v;
+	vector v = new_vector();
 	for (size_t i = 0; i < len; i++)
 	{
 		push_back(&v, data[i]);
@@ -115,8 +134,13 @@ decimal to_decimal(long long a) {
 	return d;
 }
 
+void delete_decimal(decimal* d) {
+	delete_vector(&(d->data));
+	d->sign = 0;
+}
+
 // a > b = 1   |   a < b = -1   |   a == b = 0
-short compare(decimal* a, decimal* b) {
+short compare_decimal(decimal* a, decimal* b) {
 	if (a->sign == 1 && b->sign == -1) {
 		return 1;
 	}
@@ -196,7 +220,7 @@ decimal d_sum(decimal* d1, decimal* d2) {
 
 decimal d_sub(decimal* d1, decimal* d2) {
 	decimal d3 = new_decimal();
-	short comp = compare(d1, d2);
+	short comp = compare_decimal(d1, d2);
 	if (comp == 1)
 	{
 		d3.sign = 1;
@@ -212,7 +236,7 @@ decimal d_sub(decimal* d1, decimal* d2) {
 			}
 			d3.data.v_data[i] -= d2->data.v_data[i];
 		}
-		while (d3.data.v_data[d3.data.loaded - 1] == 0 && d3.data.loaded > 1) {
+		while (d3.data.v_data[d3.data.loaded - 1] == 0) {
 			pop_back(&d3);
 		}
 		return d3;
@@ -231,7 +255,7 @@ decimal d_sub(decimal* d1, decimal* d2) {
 			}
 			d3.data.v_data[i] -= d1->data.v_data[i];
 		}
-		while (d3.data.v_data[d3.data.loaded - 1] == 0 && d3.data.loaded > 1) {
+		while (d3.data.v_data[d3.data.loaded - 1] == 0) {
 			pop_back(&d3);
 		}
 		return d3;
@@ -242,17 +266,46 @@ decimal d_sub(decimal* d1, decimal* d2) {
 	}
 }
 
+decimal d_mul(decimal* d1, decimal* d2) {
+	decimal tmp1;
+	for (size_t i = 0; i < d2->data.loaded; i++)
+	{
+		tmp1 = new_decimal();
+		decimal* ptr1 = &tmp1;
+		for (size_t j = 0; j < d2->data.v_data[i]; j++)
+		{
+			tmp1 = d_sum(&tmp1, d1);
+			print_decimal(&tmp1);
+		}
+		for (size_t k = 0; k < i; k++)
+		{
+			//push_fwd(&tmp2, 0);
+		}
+	}
+	
+	return tmp1;
+}
+
 //DECIMAL END
 
 #endif
 
 int main() {
-	decimal d1 = to_decimal(4825741578143);
-	decimal d2 = to_decimal(4225741578142);
+	decimal d1 = to_decimal(2);
+	decimal d2 = to_decimal(2);
 	print_decimal(&d1);
 	print_decimal(&d2);
-	decimal d3 = d_sub(&d2, &d1);
-	print_decimal(&d3);
-
+	//decimal d3 = d_mul(&d2, &d1);
+	//print_decimal(&d3);
+	decimal d = to_decimal(1);
+	for (size_t i = 0; i < 65536; i++)
+	{
+		d = d_sum(&d, &d);
+		print_decimal(&d);
+		printf("%d\n", &d);
+	}
+	print_decimal(&d);
+	system("pause");
+	delete_decimal(&d);
 	system("pause");
 }
